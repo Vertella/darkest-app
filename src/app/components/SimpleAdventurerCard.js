41ce4d12aeb3f8433f6c967039be90a549e969bd
Vertella <128084486+Vertella@users.noreply.github.com>
2024-09-16 17:27:07 +0200

    Flip card animation

--- src/app/components/SimpleAdventurerCard.js
+++ src/app/components/SimpleAdventurerCard.js
@@ -1,18 +1,40 @@
 // src/components/SimpleAdventurerCard.js
+import React, { useState } from "react";
+
 const SimpleAdventurerCard = ({ adventurer }) => {
-    return (
-      <div className="border border-gray-600 rounded-lg overflow-hidden shadow-md text-white bg-zinc-900 p-4 max-w-40 max-h-40 ">
-        <img
-          src={adventurer.image}
-          alt={adventurer.class}
-          className="object-cover self-center w-full"
-        />
-        <div className="flex-grow text-center">
-          <h2 className="text-l font-semibold">{adventurer.class}</h2>
+  const [flipped, setFlipped] = useState(true);
+
+  const handleCardClick = () => {
+    setFlipped(!flipped);
+  };
+
+  return (
+    <div
+      className={`relative w-40 h-40 perspective-1000 cursor-pointer overflow-hidden rounded-lg ${
+        flipped ? "flipped" : ""
+      }`}
+      onClick={handleCardClick}
+    >
+      <div className="absolute w-full h-full transition-transform duration-500 transform-style-preserve-3d">
+        {/* Front Face */}
+        <div className="absolute w-full h-full bg-zinc-700 flex items-center justify-center">
+          <img
+            src={adventurer.image}
+            alt={adventurer.class}
+            className="object-cover w-full h-full p-2"
+          />
+        </div>
+        {/* Back of the card */}
+        <div className="absolute w-full h-full bg-zinc-700 text-white flex items-center justify-center transform rotate-y-180 backface-hidden">
+          <div className="flex flex-col">
+          <p className="p-4 font-lg">{adventurer.class}</p>
+          <p className="p-4 font-light">{adventurer.description}</p>
+          </div>
+          
         </div>
       </div>
-    );
-  };
-  
-  export default SimpleAdventurerCard;
-  
+    </div>
+  );
+};
+
+export default SimpleAdventurerCard;
